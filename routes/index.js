b88c39ec6da176dd7cbd11a9cefebcecde4f28f3
Jeff Au <jeffau13@gmail.com>
2019-10-29 23:20:27 -0400

    endpoint for getting monthly schedule

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const { getMonth } = require('../handlers/schedule');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// get schedule of a given month
+router.get('/month/:month', getMonth);
 
 module.exports = router;

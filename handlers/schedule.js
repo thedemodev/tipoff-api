b5d4bec1e77662c82fd6402f095bcafa9ca5f957
Jeff Au <jeffau13@gmail.com>
2019-11-05 16:17:27 -0500

    helper method to make sure param matches data

--- handlers/schedule.js
+++ handlers/schedule.js
@@ -2,10 +2,16 @@ const schedule = require('../data/schedule.json');
 
 exports.getMonth = (req, res) => {
   let monthData = {};
-  const monthParam = req.params.month;
+  const monthParam = capitalize(req.params.month);
   monthData = schedule.lscd.filter(month => {
     return month.mscd.mon === monthParam;
   });
   //   returning one month
   res.json(monthData[0].mscd.g);
+};
+
+//helper to capitalize string
+const capitalize = s => {
+  if (typeof s !== 'string') return '';
+  return s.charAt(0).toUpperCase() + s.slice(1);
 };
